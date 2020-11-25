4fb0265b25273dd110150a9013d512703106ddfa
Kaleb Byrum <33094708+kabyru@users.noreply.github.com>
2020-11-25 11:29:53 -0500

    Add files via upload

---
+++ TimePostProcessing.ps1
@@ -0,0 +1,3 @@
+# https://geektechstuff.com/2019/09/25/installing-powershell-core-on-raspbian-rasbperry-pi/ for setting up PowerShell on RPi
+
+Measure-Command { cargo run --release | Out-Default } | Out-File ./results.txt
